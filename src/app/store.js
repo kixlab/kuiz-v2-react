851023efb8c4a77ecdd89322238305665583a9bd
greenina <igreen0485@kaist.ac.kr>
2022-08-03 13:50:41 +0900

    Add store for react-redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import optionReducer from '../features/optionSelection/optionSlice'
 import pageStatReducer from '../features/optionSelection/pageStatSlice'
+import objectiveReducer from '../features/questionStem/objectiveSlice'
 
 
 export default configureStore({
   reducer: {
       option: optionReducer,
-      pageStat: pageStatReducer
+      pageStat: pageStatReducer,
+      objective : objectiveReducer
   },
 })
