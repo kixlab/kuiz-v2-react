cfa3c059935097f329cf70706a810a5f0cecfaa2
greenina <igreen0485@kaist.ac.kr>
2022-08-14 21:22:18 +0900

    Apply persist in redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,58 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import optionReducer from '../features/optionSelection/optionSlice'
 import pageStatReducer from '../features/optionSelection/pageStatSlice'
 import objectiveReducer from '../features/questionStem/objectiveSlice'
 import userReducer from '../features/authentication/userSlice'
 import postReducer from '../features/post/postSlice'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
 
-export default configureStore({
-  reducer: {
-      option: optionReducer,
-      pageStat: pageStatReducer,
-      objective : objectiveReducer,
-      userInfo : userReducer,
-      post : postReducer
-  },
-})
+const rootReducer = combineReducers({
+    option: optionReducer,
+    pageStat: pageStatReducer,
+    objective : objectiveReducer,
+    userInfo : userReducer,
+    post : postReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+      // }).concat(logger),
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
+
+// export default configureStore({
+//   reducer: {
+//       option: optionReducer,
+//       pageStat: pageStatReducer,
+//       objective : objectiveReducer,
+//       userInfo : userReducer,
+//       post : postReducer
+//   },
+// })
