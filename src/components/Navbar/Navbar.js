13c83135dbee935d127b4f4793e07779839b601b
greenina <igreen0485@kaist.ac.kr>
2022-08-15 19:20:51 +0900

    Enabled navigation in navigation bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,24 +2,35 @@ import "./Navbar.scss";
 import { useLocation } from "react-router";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 import Button from "../Button/Button";
-const withouSidebarRoutes = ["/login"];
+
 
 function Navbar() {
     // const {pathname} = useLocation();
     // if (withouSidebarRoutes.some((item) => pathname.includes(item))) return null;
     const profile = useSelector((state)=>state.userInfo.userInfo.imageUrl)
     const cid = useSelector((state) => state.userInfo.cid)
+    const navigate = useNavigate()
+    const moveToCreateStem = () => {
+        navigate("/"+cid+"/createstem")
+    }
+    const moveToCreateOption = () => {
+        navigate("/"+cid)
+    }
+    const moveToQlist = () => {
+        navigate("/"+cid+"qlist")
+    }
 
 	return (
         <div id="left-sidebar">
             <div id="main-logo">KUIZ</div>
             {cid}
             <div id="side-nav">
-                <Button>Create Stem</Button>
-                <Button>Create Option</Button>
-                <Button>Question List</Button>
+                <Button onClick={moveToCreateStem}>Create Stem</Button>
+                <Button onClick={moveToCreateOption}>Create Option</Button>
+                <Button onClick={moveToQlist}>Question List</Button>
             </div>
             <div className="profile"><img src={profile}/></div>
         </div>
