35da614ca93bbb6abc4dfe5f591931af9a5a3073
greenina <igreen0485@kaist.ac.kr>
2022-08-16 22:45:19 +0900

    Add relative time

--- src/components/QuestionListItem/QuestionListItem.js
+++ src/components/QuestionListItem/QuestionListItem.js
@@ -1,11 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./QuestionListItem.scss";
+import dayjs from 'dayjs'
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 
 const QuestionList = (props) => {
-	useEffect(() => {
-		// console.log("PROPS:",props)
-	},[])
+	
+	const formatDate = (date) => {
+		if (date === "") {
+		  setTimeout(formatDate, 100);
+		} else {
+		  const createdAt = new Date(date);
+		  const now = new Date();
+		  const diff = (now.getTime() - createdAt.getTime()) / 60000;
+		  const diffInt = Math.trunc(diff);
+
+		  if (diffInt > 10080) {
+			const dateStringArray = createdAt.toString().split(" ");
+			const mmm = dateStringArray[1];
+			const dd = dateStringArray[2];
+			const HH = dateStringArray[4].substring(0, 2); // Hour in 24-hour format
+			const hh = HH > 12 ? String(HH - 12) : HH; // Hour in 12-hour format
+			const mm = dateStringArray[4].substring(3, 5);
+			const period = HH > 11 ? "PM" : "AM";
+			const exactDate =
+			  mmm + " " + dd + " at " + hh + ":" + mm + " " + period;
+			// setDateFormatted(exactDate)
+			return exactDate
+		  } else {
+			// setDateFormatted(dayjs(date).fromNow())
+			return dayjs(date).fromNow()
+		  }
+		}
+	  }
 	return (
 		<div className="question-list-item">
 			<div className="question-list-number">{props.number}</div>
@@ -13,7 +41,7 @@ const QuestionList = (props) => {
 			<div className="question-list-optioncount">
 				{props.options && props.options.length}
 			</div>
-			<div className="question-list-date">{props.date}</div>
+			<div className="question-list-date">{formatDate(props.date)}</div>
 		</div>
 	);
 }
