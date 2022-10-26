1050e589c21e00248ca9f36c35df406d09eb77d0
Haesoo Kim <haesoo1108@gmail.com>
2022-10-26 18:02:42 +0900

    Add shuffle function and fix answer display error

--- src/pages/Question/Question.js
+++ src/pages/Question/Question.js
@@ -19,172 +19,76 @@ const Question = (props) => {
 	const cid = useParams().cid;
 	const [selected, setSelected] = useState();
 	const cType = useSelector((state) => state.userInfo.cType);
-	const [answer, setAnswer] = useState();
+	const [answer, setAnswer] = useState(0);
 	const uid = useSelector((state) => state.userInfo.userInfo._id);
 	const [isOptionValid, setIsOptionValid] = useState();
 	const [isSolved, setIsSolved] = useState();
+
+	const [ans, setAns] = useState([]);
+	const [dis, setDis] = useState([]);
+
 	function getMultipleRandom(arr, num) {
 		const shuffled = [...arr].sort(() => 0.5 - Math.random());
-
 		return shuffled.slice(0, num);
 	}
+
 	function shuffle(array) {
 		array.sort(() => Math.random() - 0.5);
 		return array;
 	}
 	function removeById(array, id) {
-		var index = array.map(x => {
-			return x._id;
-		}).indexOf(id);
-		array.splice(index, 1)
+		var index = array
+			.map((x) => {
+				return x._id;
+			})
+			.indexOf(id);
+		array.splice(index, 1);
 		// const array2 = array.filter((a, i) => i !== index)
 		return array;
 	}
 
 	const getQinfo = (qid) => {
-		axios
-			.get(
-				`${process.env.REACT_APP_BACK_END}/question/detail/load?qid=` +
-					qid
-			)
-			.then((res) => {
-				if (cType) {
-					if (res.data.data.qinfo.cluster.length < 3) {
-						setIsOptionValid(false);
-					} else {
-						axios
-							.post(
-								`${process.env.REACT_APP_BACK_END}/question/load/clusters`,
-								{
-									clusters: res.data.data.qinfo.cluster,
-								}
-							)
-							.then((res2) => {
-								const clusters = res2.data.clusters;
-								var ans = clusters.filter((c) => c.ansExist)
-								var dis = clusters.filter((c) => c.disExist)
-								var ov = clusters.filter((c) => c.ansExist && c.disExist)
-								
-								if(ans.length + dis.length - ov.length >=4 && dis.length>=3){
-									if(ans.length - ov.length > 0 && dis.length < 4) {
-										var ansList = getMultipleRandom(ans.filter(a => !a.disExist), 1)
-										var disList = getMultipleRandom(dis, 3)
-										setOptions(shuffle(ansList.map(a => a.ansRep).concat(disList.map(d => d.disRep))))
-										setIsOptionValid(true)
-									} else {
-										var ansList = getMultipleRandom(ans,1)
-										dis = removeById(dis, ansList[0]._id)
-										var disList = getMultipleRandom(dis, dis.length < 4 ? 3 : 4);
-										setOptions(shuffle(ansList.map(a => a.ansRep).concat(disList.map(d => d.disRep))))
-										setIsOptionValid(true)
-									}
-								} else {
-									setIsOptionValid(false)
-								}
-								// if (clusters.filter((c) => c.ansExist && !c.disExist).length >= 1) {
-								// 	if(clusters.filter((c) => c.disExist === true).length >= 3){
+		let optionList;
+		axios.get(`${process.env.REACT_APP_BACK_END}/question/detail/load?qid=` + qid).then((res) => {
+			axios
+				.post(`${process.env.REACT_APP_BACK_END}/question/load/clusters`, {
+					clusters: res.data.data.qinfo.cluster,
+				})
+				.then((res2) => {
+					const clusters = res2.data.clusters;
+					var ans = clusters.filter((c) => c.ansExist);
+					var dis = clusters.filter((c) => c.disExist);
 
-								// 	} else {
-								// 		if(clusters.filter((c) => c.disExist === true).length == 3) {
+					var ansList = getMultipleRandom(ans, 1);
+					var disList = getMultipleRandom(dis, 3);
 
-								// 		} else {
-								// 			setIsOptionValid(false)
-								// 		}
-								// 	}
-								// 	setIsOptionValid(true);
+					optionList = shuffle(ansList.map((a) => a.ansRep).concat(disList.map((d) => d.disRep)));
 
-								// 	// const ansList1 = clusters
-								// 	// 	.filter((c) => c.ansExist === true)
-								// 	// 	.map((c2) => c2.ansList);
-								// 	// const disList1 = clusters
-								// 	// 	.filter((c) => c.disExist === true)
-								// 	// 	.map((c2) => c2.disList);
-								// 	// const ansList2 = [].concat.apply([], ansList1);
-								// 	// const disList2 = [].concat.apply([], disList1);
-								// 	const ansList2 = clusters.filter(c => c.ansExist).map(c2 => c2.ansRep)
-								// 	console.log("ansList2:", ansList2)
-								// 	const disList2 = clusters.filter(c => c.disExist).map(c2 => c2.disRep)
-								// 	console.log("disList2:", disList2)
-								// 	const answer = getMultipleRandom(ansList2, 1);
-								// 	const distractor = getMultipleRandom(
-								// 		disList2,
-								// 		disList2.length < 3 ? disList2.length : 3
-								// 	);
-								// 	setOptions(shuffle(answer.concat(distractor)));
-
-								// 	// axios
-								// 	// 	.post(
-								// 	// 		`${process.env.REACT_APP_BACK_END}/question/load/options`,
-								// 	// 		{
-								// 	// 			optionList: ansList2,
-								// 	// 		}
-								// 	// 	)
-								// 	// 	.then((res3) => {
-								// 	// 		const ansList = res3.data.options;
-								// 	// 		axios
-								// 	// 			.post(
-								// 	// 				`${process.env.REACT_APP_BACK_END}/question/load/options`,
-								// 	// 				{
-								// 	// 					optionList: disList2,
-								// 	// 				}
-								// 	// 			)
-								// 	// 			.then((res4) => {
-								// 	// 				const disList = res4.data.options;
-
-								// 	// 				/* selection algorithm of ansList should be here. random selection for now */
-								// 	// 				const answer = getMultipleRandom(ansList, 1);
-								// 	// 				const distractor = getMultipleRandom(
-								// 	// 					disList,
-								// 	// 					disList.length < 3 ? disList.length : 3
-								// 	// 				);
-
-								// 	// 				//shuffle array
-								// 	// 				setOptions(shuffle(answer.concat(distractor)));
-								// 	// 			});
-								// 	// 	});
-								// } else {
-								// 	if(
-								// 		clusters.filter((c) => c.ansExist && !c.disExist).length === 0 &&
-								// 		clusters.filter((c) => c.disExist === true).length >= 3
-								// 	) {
-
-								// 		setIsOptionValid(true)
-								// 	} else {
-								// 		setIsOptionValid(false);
-								// 	}
-									
-								// }
-							})
-							.catch((err) => console.log(err));
-					}
-				} else {
+					setOptions(optionList);
 					setIsOptionValid(true);
-				}
 
-				setQinfo(res.data.data.qinfo);
-				setOptions(res.data.data.options)
-				res.data.data.options.map((o, i) => {
-					if (o.is_answer) {
-						setAnswer(i);
-					}
-				});
-			});
+					optionList.map((o, i) => {
+						if (o.is_answer) {
+							setAnswer(i);
+						}
+					});
+				})
+				.catch((err) => console.log(err));
+			setQinfo(res.data.data.qinfo);
+		});
 	};
-	// getQinfo(qid)
+
 	const isLoggedIn = useSelector((state) => state.userInfo.isLoggedIn);
 	const checkAnswer = () => {
 		if (!ansVisible) {
 			axios
-				.post(
-					`${process.env.REACT_APP_BACK_END}/question/solve`,
-					{
-						qid: qid,
-						uid: uid,
-						initAns: options[selected]._id,
-						isCorrect: selected === answer,
-						optionSet: options.map((o) => ObjectID(o._id)),
-					}
-				)
+				.post(`${process.env.REACT_APP_BACK_END}/question/solve`, {
+					qid: qid,
+					uid: uid,
+					initAns: options[selected]._id,
+					isCorrect: selected === answer,
+					optionSet: options.map((o) => ObjectID(o._id)),
+				})
 				.then((res) => {
 					console.log("success:", res.data.success);
 				});
@@ -193,19 +97,19 @@ const Question = (props) => {
 	};
 
 	const background = (index) => {
-		if(!ansVisible){
-			return 
+		if (!ansVisible) {
+			return;
 		} else {
-			if(index === answer){
-				return "answer"
+			if (index === answer) {
+				return "answer";
 			} else {
-				if(index === selected){
-					return "wrong-selected"
+				if (index === selected) {
+					return "wrong-selected";
 				}
 			}
-			return "other"
+			return "other";
 		}
-	}
+	};
 	useEffect(() => {
 		if (isLoggedIn) {
 			getQinfo(qid);
@@ -214,17 +118,21 @@ const Question = (props) => {
 		}
 	}, []);
 
+	const shuffleOptions = () => {
+		getQinfo(qid);
+		setIsSolved(false);
+		setSelected();
+		setAnsVisible(false);
+	};
+
+	const reportErr = () => {};
 
 	return (
 		<div id="question-screen-wrapper">
-			<div id="question-nav"></div>
 			<div id="question-screen">
-				<Link
-					to={"/" + cid + "/qlist"}
-					style={{ textDecoration: "none", color: "#000000" }}
-				>
+				<Link to={"/" + cid + "/qlist"} style={{ textDecoration: "none", color: "#000000" }}>
 					<div id="return-button">
-						<i className="fa-solid fa-arrow-left"></i> 목록으로 돌아가기
+						<i className="fa-solid fa-arrow-left"></i> Return to Question List
 					</div>
 				</Link>
 				{qinfo && (
@@ -235,78 +143,60 @@ const Question = (props) => {
 						className="introduce-content"
 					/>
 				)}
-				{isOptionValid ? (
-					<div id="question-options">
-						{options &&
-							options.map((option, index) => (
-								<div className="question-option-item" id={background(index)}>
-									<input
-										key={index}
-										checked={selected === index}
-										type="radio"
-										onChange={(e) => {
-											setSelected(index);
-											setIsSolved(true);
-										}}
-										name={option.option_text}
-									/>
-									<label htmlFor={option.option_text}>
-										{option.option_text}
-									</label>
-								</div>
-							))}
-					</div>
-				) : (
-					<div>Not enough options yet</div>
-				)}
-				{isOptionValid ? (
-					<div id="question-explanation">
-						<button
-							id="hide-answer"
-							onClick={() => checkAnswer()}
-							disabled={!isSolved}
-						>
-							정답 확인하기
-							{ansVisible ? (
-								<i className="fa-solid fa-chevron-up"></i>
-							) : (
-								<i className="fa-solid fa-chevron-down"></i>
-							)}
-						</button>
-						{ansVisible &&
-							(cType ? (
-								<div id="option-wrapper">
-									<div>answer: {options[answer].option_text}</div>
-									{options.map((option) => {
-										return (
-											<div className="answer-option">
-												<div className="option-text">{option.option_text}</div>
-												<div className="option-exp">{option.explanation}</div>
-											</div>
-										);
-									})}
-								</div>
-							) : (
-								<div id="explanation">
-									<div className="ans-check">
-										{(selected === answer)?<div className="correct">정답입니다!</div>:<div className="incorrect">오답입니다!</div>}
+
+				<div id="question-options">
+					{options &&
+						options.map((option, index) => (
+							<div className="question-option-item" id={background(index)} key={index}>
+								<input
+									key={index}
+									checked={selected === index}
+									type="radio"
+									onChange={(e) => {
+										setSelected(index);
+										setIsSolved(true);
+									}}
+									id={option.option_text}
+								/>
+								<label htmlFor={option.option_text}>{option.option_text}</label>
+							</div>
+						))}
+				</div>
+
+				<div id="question-explanation">
+					<button id="hide-answer" onClick={() => checkAnswer()} disabled={!isSolved}>
+						Check Answer
+						{ansVisible ? (
+							<i className="fa-solid fa-chevron-up"></i>
+						) : (
+							<i className="fa-solid fa-chevron-down"></i>
+						)}
+					</button>
+					<button id="shuffle-answer" onClick={() => shuffleOptions()}>
+						Shuffle Answers
+					</button>
+
+					<button id="report-error" onClick={() => shuffleOptions()}>
+						Report Question Error
+					</button>
+					{ansVisible && (
+						<div id="option-wrapper">
+							<div>answer: {options[answer].option_text}</div>
+							{options.map((option) => {
+								return (
+									<div className="answer-option">
+										<div className="option-text">{option.option_text}</div>
+										<div className="option-exp">{option.explanation}</div>
 									</div>
-									<div>정답 : {options[answer].option_text}</div>
-									<hr/>
-									해설 : {qinfo.explanation}
-								</div>
-							))}
-					</div>
-				) : (
-					<div></div>
-				)}
-				{cType ? (
-					<Link to={"/" + cid + "/question/" + qid + "/create"}>
-						<button className="nav-button">선택지 추가하기</button>
-					</Link>
-				) : (
-					<></>
-				)}
+								);
+							})}
+						</div>
+					)}
+				</div>
+
+				<Link to={"/" + cid + "/question/" + qid + "/create"}>
+					<button className="nav-button">선택지 추가하기</button>
+				</Link>
 			</div>
 		</div>
 	);
