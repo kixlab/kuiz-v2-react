83636944deed385f574e0efddec728b31bc4ea86
Haesoo Kim <haesoo1108@gmail.com>
2022-10-26 15:30:44 +0900

    Remove unused elements and organic option

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import QuestionList from "./pages/QuestionList/QuestionList";
 import Question from "./pages/Question/Question";
-import {
-	BrowserRouter,
-	Route,
-	Routes,
-	useSearchParams,
-	Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StemCreate from "./pages/StemCreate/StemCreate";
-import StemCreate2 from "./organicPages/CreateStem2/CreateStem2";
+// import StemCreate2 from "./organicPages/CreateStem2/CreateStem2";
 import OptionCreate from "./pages/OptionCreate/OptionCreate";
 import QuestionListOption from "./pages/QuestionListOption/QuestionListOption";
 import Login from "./pages/Login/Login";
@@ -19,72 +13,41 @@ import Enroll from "./pages/Enroll/Enroll";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Kakao from "./pages/Kakao/Kakao";
 import "./App.scss";
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate, useParams } from "react-router";
-import { enrollClass } from "./features/authentication/userSlice";
-import axios from "axios";
+import React from "react";
+import { useSelector } from "react-redux";
+// import { useNavigate, useParams } from "react-router";
+// import { enrollClass } from "./features/authentication/userSlice";
+// import axios from "axios";
 
 function App() {
 	const isAdmin = useSelector((state) => state.userInfo.userInfo.isAdmin);
-	const cType = useSelector((state) => state.userInfo.cType)
+	const cType = useSelector((state) => state.userInfo.cType);
 
 	return (
 		<BrowserRouter>
 			<div className="app-wrapper">
-					<Navbar/>
-					<main>
-						<Routes>
-							{cType && (
-								<Route
-									path="/:cid"
-									element={<QuestionListOption  classType={cType} />}
-								/>
-							)}
+				<Navbar />
+				<main>
+					<Routes>
+						{cType && <Route path="/:cid" element={<QuestionListOption classType={cType} />} />}
+						<Route path="/:cid/qlist" element={<QuestionList classType={cType} />} />
+						<Route path="/kakaologin" element={<Kakao classType={cType} />} />
+						<Route path="/:cid/question/:id" element={<Question classType={cType} />} />
+						<Route path="/:cid/createstem" element={<StemCreate classType={cType} />} />
+						{cType && (
 							<Route
-								path="/:cid/qlist"
-								element={<QuestionList classType={cType}/>}
+								path="/:cid/question/:id/create"
+								element={<OptionCreate classType={cType} />}
 							/>
-							<Route
-								path="/kakaologin"
-								element={<Kakao classType={cType}/>}
-							/>
-							<Route
-								path="/:cid/question/:id"
-								element={<Question classType={cType}/>}
-							/>
-							<Route
-								path="/:cid/createstem"
-								element={
-									cType ? (
-										<StemCreate classType={cType}/>
-									) : (
-										<StemCreate2 classType={cType}/>
-									)
-								}
-							/>
-							{cType && (
-								<Route
-									path="/:cid/question/:id/create"
-									element={<OptionCreate classType={cType}/>}
-								/>
-							)}
-							<Route path="/login" element={<Login classType={cType}/>} />
-							<Route path="/enroll" element={<Enroll classType={cType}/>} />
-							<Route
-								path="/:cid/mypage"
-								element={<MyPage classType={cType}/>}
-							/>
-							{isAdmin && (
-								<Route
-									path="/:cid/admin"
-									element={<Admin  classType={cType}/>}
-								/>
-							)}
-							<Route path="*" element={<PageNotFound/>}/>
-						</Routes>
-					</main>
-				</div>
+						)}
+						<Route path="/login" element={<Login classType={cType} />} />
+						<Route path="/enroll" element={<Enroll classType={cType} />} />
+						<Route path="/:cid/mypage" element={<MyPage classType={cType} />} />
+						{isAdmin && <Route path="/:cid/admin" element={<Admin classType={cType} />} />}
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</main>
+			</div>
 		</BrowserRouter>
 	);
 }
